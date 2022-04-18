bcd864d496c3d97a538c9ae7a587152eb40400c2
Eldin Kecap <eldinkecap@gmail.com>
2022-04-18 19:22:16 +0200

    11.uradio

--- Zadatak54./zadatak11.js
+++ Zadatak54./zadatak11.js
@@ -1,13 +1,23 @@
 function ispisTabMnozenja (){
     let arr = []; 
-    let arr2d = [];
+    let row = '';
     
     for (let i = 1; i < 10; i++) {
         for (let j = 1; j < 10; j++) {
-            arr[i] = i*j;          
-        }        
-        arr2d.push(arr);
+            arr.push(i*j);          
+        }   
+        for (let i = 0; i < arr.length; i++) {
+            if(arr[i]<10){
+                let temp = ' ' + arr[i];
+                arr[i]= temp;
+            }
+            
+        } 
+        row = String(arr);
+        row = row.replace(/,/g,' ');
+        arr = [];  
+        console.log(row);
     }
-    console.table(arr2d);
+    
 }
 ispisTabMnozenja();
