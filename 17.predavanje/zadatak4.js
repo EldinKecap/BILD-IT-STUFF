95e4f83c42d6c60ade68d488aebc48fbe5ea2818
Eldin Kecap <eldinkecap@gmail.com>
2022-06-16 15:37:57 +0200

    Zavrsio zadatke pred 17

--- 17.predavanje/zadatak4.js
+++ 17.predavanje/zadatak4.js
@@ -21,29 +21,60 @@ function recurIndex(str){
     if (!str) {
         return obj;
     }
+    //ubacim u objekat sva slova
     for(let i of str){
         obj[i]=null;
     }
     let start=0;
     // console.log(obj)
+    // dodjelim niz koji sadrzi prvi index i drugi index na kojem se pojavilo slovo
+    next:
     for (const key in obj) {
         for (let i = 0; i < str.length; i++) {
             if (key === str[i]) {
                 counter++
-                if (start === 0) {
-                    start = i  
+                // console.log(i)
+                if (start === 0 && i!=0 && counter === 1) {
+                    start = i ;
+                    // console.log(start,key) 
                 }
                 if (counter===2) {
                     counter = 0;
                     obj[key]=[start,i];
+                    start = 0;
+                    continue next
                 }
             }
             
-        }    
+        } 
+        counter = 0;   
+    }
+    let min = Infinity;
+    // odredim najmanji drugi index 
+    for (const key in obj) {
+        if(obj[key]!=null){
+            if(obj[key][1]<min){
+                min = obj[key][1];
+                // console.log(key,obj[key][0],obj[key][1],min)
+            }
+        }
+    }
+    // brisem propertyje koji nisu sa najmanjim zadnjim indexom
+    for (const key in obj) {
+        if (obj[key]===null) {
+            delete obj[key];
+        }
+        if (obj[key]!==undefined) {
+            if (obj[key][1]!=min) {
+                delete obj[key];
+                
+            }
+        }
+       
     }
     return obj
 }
-console.log(recurIndex('DXTDXTXDTXD'))
-// console.log(recurIndex("YXZXYTUVXWV")); 
-// console.log(recurIndex("YZTTZMNERXE"));
-// console.log(recurIndex("AREDCBSDERD"));
+console.log(recurIndex('DXTDXTXDTXD'));
+console.log(recurIndex("YXZXYTUVXWV")); 
+console.log(recurIndex("YZTTZMNERXE"));
+console.log(recurIndex("AREDCBSDERD"));
